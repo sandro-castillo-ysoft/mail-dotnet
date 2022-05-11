32719b2c2bc1df92a5ad49c89120c74c68219947
Sandro Castillo <sandro.castillo@ysoft.com>
2022-05-11 12:40:25 -0500

    test

--- tests/mail-dotnetTests/Mailing/SMTP/TestVariables.cs
+++ tests/mail-dotnetTests/Mailing/SMTP/TestVariables.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace mail_dotnetTests.Mailing.SMTP {
     internal class TestVariables {
-        internal static readonly string HOST = "smtp.server.com";
+        internal static readonly string HOST = "smtp.server.com1";
         internal static readonly short PORT = 25;
         internal static readonly string SSL_OPTION = "STARTTLS";
         internal static readonly string USERNAME = "username@mail.com";
