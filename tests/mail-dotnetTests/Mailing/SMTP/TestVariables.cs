d01164db4d80f27f2aac467e3735369bb1be5198
Sandro Castillo <sandro.castillo@ysoft.com>
2022-05-11 12:49:15 -0500

    tracked files back

---
+++ tests/mail-dotnetTests/Mailing/SMTP/TestVariables.cs
@@ -0,0 +1,11 @@
+﻿
+namespace mail_dotnetTests.Mailing.SMTP {
+    internal class TestVariables {
+        internal static readonly string HOST = "smtp.server.com";
+        internal static readonly short PORT = 25;
+        internal static readonly string SSL_OPTION = "STARTTLS";
+        internal static readonly string USERNAME = "username@mail.com";
+        internal static readonly string PASSWORD = "P@$$w0rd!";
+
+    }
+}
