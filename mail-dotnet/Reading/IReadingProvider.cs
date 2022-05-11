7a2c3a1990c3eb08eadac9cd7e5aba480b26f10a
Sandro Castillo <sandro.castillo@ysoft.com>
2022-05-11 10:06:28 -0500

    Added simple layout scope for reading mail

---
+++ mail-dotnet/Reading/IReadingProvider.cs
@@ -0,0 +1,13 @@
+﻿using MimeKit;
+
+namespace mail_dotnet.Reading {
+    public interface IReadingProvider {
+        /// <returns>List of message's Unique IDs</returns>
+        public string[] GetInboxList(bool unreadOnly = true);
+        /// <returns>Filtered Inbox List</returns>
+        public string[] GetInboxList(string fromRegex = ".*", string subjectRegex = ".*", bool unreadOnly = true);
+        /// <returns>List of message's Subjects in inbox</returns>
+        public string[] GetInboxListSubjects();
+        public MimeMessage GetMail(string id, bool markRead = true, bool deleteAfter = true);
+    }
+}
---
+++ mail-dotnet/Reading/ReadingProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+using MimeKit;
+
+namespace mail_dotnet.Reading {
+    public abstract class ReadingProvider : MailProvider, IReadingProvider {
+        protected ReadingProvider(ILogger? _logger) : base(_logger) {
+        }
+
+        public abstract string[] GetInboxList(bool unreadOnly = true);
+        public abstract string[] GetInboxList(string fromRegex = ".*", string subjectRegex = ".*", bool unreadOnly = true);
+        public abstract string[] GetInboxListSubjects();
+        public abstract MimeMessage GetMail(string id, bool markRead = true, bool deleteAfter = true);
+    }
+}
